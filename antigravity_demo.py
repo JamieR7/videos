b6c5dbee3ca58fbd87749bca0d5293117eecf0fb
JamieR <thatjamiebloke@gmail.com>
2026-01-27 22:48:48 +0100

    Create antigravity_demo.py

---
+++ antigravity_demo.py
@@ -0,0 +1,12 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2822
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 .SFNSMono-Light_YAXS140B3B6_wght1C20000;}
+{\colortbl;\red255\green255\blue255;\red96\green155\blue252;\red69\green83\blue77;}
+{\*\expandedcolortbl;;\cssrgb\c44706\c68235\c99216;\cssrgb\c34118\c39608\c37647\c20000;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs28 \cf2 \cb3 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 import antigravity\
+print("Check your browser for xkcd 353!")\
+}
